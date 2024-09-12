c078ac7001af90b04f7a585b0b0aadd770821a41
Javilunaale <javierluna.ale@gmail.com>
2024-09-12 19:01:15 -0300

    agregado de imagen

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,15 +3,10 @@ import Cartwidget from './Cartwidget'
 
 const NavBar = () => {
     return (
-        /*<nav className="">
-            <button type="button" className="btn btn-outline-primary">Pet Shop "LOLI Y PANCHA"</button>
-            <button type="button" className="btn btn-outline-primary">Perros</button>
-            <button type="button" className="btn btn-outline-primary">Gatos</button>
-            <button type="button" className="btn btn-outline-primary">Accesorios</button>
-            <button type="button" className="btn btn-outline-primary">Salud</button>
-            </nav>*/
+     
         <nav className="navbar bg-body-tertiary">
             <form className="container-fluid justify-content-start">
+            <img src="https://rediredi.com/wp-content/uploads/elementor/thumbs/logo_pet-qoub2ynqqhmn6xvicicm9ybwz0khlgpghspjqcvlz4.webp" alt="logo" />
                 <button className="btn btn-outline-success me-2" type="button">Pet Shop "LOLI Y PANCHA"</button>
                 <button className="btn btn-sm btn-outline-secondary" type="button">Perros</button>
                 <button className="btn btn-sm btn-outline-secondary" type="button">Gatos</button>
